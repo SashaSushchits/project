54b40cd5b4f98054c89f316214dc2f9147b87a1f
Sasha_Sushchits <sushchitskaban@mail.ru>
2021-12-08 12:53:52 +0300

    изменение данных в хранилище, мутация

--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -33,7 +33,7 @@
         <span class="item__code">Артикул: {{ product.id }}</span>
         <h2 class="item__title">{{ product.title }}</h2>
         <div class="item__form">
-          <form class="form" action="#" method="POST">
+          <form class="form" action="#" method="POST" @submit.prevent="addToCart">
             <b class="item__price"> {{ product.price | numberFormat }} Br </b>
 
             <fieldset class="form__block">
@@ -130,15 +130,15 @@
 
             <div class="item__row">
               <div class="form__counter">
-                <button type="button" aria-label="Убрать один товар">
+                <button :class="{'pagination__link--disabled': productAmount === 1}" type="button" aria-label="Убрать один товар" v-if="productAmount !== 0" @click.prevent="productAmount--">
                   <svg width="12" height="12" fill="currentColor">
                     <use xlink:href="#icon-minus"></use>
                   </svg>
                 </button>
 
-                <input type="text" value="1" name="count" />
+                <input type="text" v-model.number="productAmount" />
 
-                <button type="button" aria-label="Добавить один товар">
+                <button type="button" aria-label="Добавить один товар" @click.prevent="productAmount++">
                   <svg width="12" height="12" fill="currentColor">
                     <use xlink:href="#icon-plus"></use>
                   </svg>
@@ -222,6 +222,11 @@
     import numberFormat from "@/helpers/numberFormat";
 
     export default {
+      data() {
+        return {
+          productAmount: 1
+        };
+      },
         filters:{
             numberFormat
         },
@@ -234,7 +239,13 @@
             }
         },
         methods: {
-            gotoPage
+            gotoPage,
+            addToCart(){
+              this.$store.commit(
+                'addProductToCart',
+                {productId: this.product.id, amount: this.productAmount}
+              )
+            }
         }
     }
 </script>
