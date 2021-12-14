24a451d1c533bebfe2d2f227073453bf60311165
Sasha_Sushchits <sushchitskaban@mail.ru>
2021-12-14 10:46:01 +0300

    Корзина товаров. Действия во vuex

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,21 @@
 <script>
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
+import { mapActions, mapMutations } from 'vuex'
+
 export default {
   components: { Header, Footer },
+  created(){
+    const userAccessKey = localStorage.getItem('userAccessKey');
+    if(userAccessKey){
+      this.updateUserAccessKey(userAccessKey)
+    }
+    this.loadCart()
+  },
+  methods: {
+    ...mapActions(['loadCart']),
+    ...mapMutations(['updateUserAccessKey'])
+  }
 };
 </script>
 
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -150,7 +150,7 @@ export default {
           .then((response) => (this.productsData = response.data))
           .catch(() => this.productsLoadingFailed = true)
           .then(() => this.productsLoading = false);
-      }, 10000)
+      }, 3000)
     },
   },
   created() {
